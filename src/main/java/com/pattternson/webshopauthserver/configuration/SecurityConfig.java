82e6efcb45245f08fe374f5900fbe7745cd04da7
TobiasPettersson <pettersson.tobias84@gmail.com>
2018-02-28 15:38:25 +0100

    init commit

---
+++ src/main/java/com/pattternson/webshopauthserver/configuration/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.pattternson.webshopauthserver.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private AuthenticationManager authenticationManager;
+
+    public SecurityConfig(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception { // @formatter:off
+        http.requestMatchers()
+                .antMatchers("/login", "/oauth/authorize", "/exit")
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .permitAll();
+    } // @formatter:on
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception { // @formatter:off
+        auth.parentAuthenticationManager(authenticationManager)
+                .inMemoryAuthentication()
+                .withUser("user")
+                .password("user")
+                .roles("USER").and()
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN");
+    } // @formatter:on
+
+}
